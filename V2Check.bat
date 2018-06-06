2fcfd61873ec81adec25ed73d6f8bb54e64af6dd
andrew-waugh <38519724+andrew-waugh@users.noreply.github.com>
2018-06-06 16:48:49 +1000

    Set up -dtd option

--- V2Check.bat
+++ V2Check.bat
@@ -1,7 +1,8 @@
 @echo off
 rem set code="C:\Users\Andrew\Documents\Work\VERS-2015\VPA"
-rem set bin="C:\Program Files\Java\jdk1.8.0_162\bin"
+rem set bin="C:\Program Files (x86)\Java\jre1.8.0_144\bin"
 set code="J:\PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS\VERS-1999\V2Check"
-set bin="C:\Program Files\Java\jdk1.8.0_144\bin"
+set bin="C:\Program Files (x86)\Java\jre1.8.0_144\bin"
+rem set bin="C:\Program Files (x86)\Java\jre1.8.0_131\bin"
 set versclasspath=%code%/dist/*
-%bin%\java -classpath %versclasspath% VEOCheck.VEOCheck -all %*
+%bin%\java -classpath %versclasspath% VEOCheck.VEOCheck -all -dtd %code%/vers.dtd %*
