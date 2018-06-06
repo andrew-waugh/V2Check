75ec99af2322617df05e1efcb7861739a0b821d0
andrew-waugh <38519724+andrew-waugh@users.noreply.github.com>
2018-06-06 17:13:10 +1000

    Minor typo

--- VEOCheck.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set code=C:\Documents and Settings\Andrew Waugh\My Documents\Work\OldVEO\VEOCheckII
-set versclasspath="%code%\build\classes"
-java -Xmx200m -classpath %versclasspath% VEOCheck.VEOCheck -extract %*
--- VEOCheckVerbose.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set code="G:PROV\TECHNOLOGY MANAGEMENT\Application Development\VERS"
-set versclasspath=%code%"
-java -Xmx200m -classpath %versclasspath% VEOCheckII.VEOCheck %*
